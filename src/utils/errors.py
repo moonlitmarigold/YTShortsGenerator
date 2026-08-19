b686cb2d5bc86c73e65bfe1f08df6f1bdfd4be44
moonlitmarigold <moonlitmarigold12@gmail.com>
2026-08-19 18:16:17 +0200

    feat: add API error types for research backends
    RequestReturned429, NoResultsAfterRequestAttempts (retry budget
    exhausted) and NoWikiquotePage, raised by the Wikiquote backend.

--- src/utils/errors.py
+++ src/utils/errors.py
@@ -18,3 +18,18 @@ class ConfigFileNotSet(Exception):
 class ConfigFileNotFound(Exception):
     def __init__(self, path):
         super().__init__(f'Cannot find config or env file in current path {path}. Either set a path via "--config" or "--env" or create the missing files.')
+
+class RequestReturned429(Exception):
+
+    def __init__(self):
+        super().__init__("The Request returned a 429 on the request")
+
+class NoResultsAfterRequestAttempts(Exception):
+
+    def __init__(self, tries):
+        super().__init__(f'Gave up after {tries} tries to make the api call')
+
+class NoWikiquotePage(Exception):
+
+    def __init__(self, title):
+        super().__init__(f'Wikiquote has no page titled "{title}"')
